3f342aef118bb9b3219bc984c3a577b4b58e311f
rbakx <rene.bakx@gmail.com>
2015-12-23 02:37:18 +0100

    Made standalone Python script compatible with Choregraphe Python script

--- PythonDemo/human_greeter.py
+++ PythonDemo/human_greeter.py
@@ -5,32 +5,62 @@
 
 import sys
 import time
+import re
 
 from naoqi import ALProxy
 from naoqi import ALBroker
 from naoqi import ALModule
 
-# Global variables
+
+# Global variables. These must be global otherwise the callback functions will report for example: 'python object not found FaceRecognizer'.
 SpeechRecognizer = None
-memorySpeech = None
-HumanGreeter = None
-memoryFace = None
+FaceRecognizer = None
+ReactToTouch = None
 
 
-class speechRecognizerModule(ALModule):
+class ReactToTouchModule(ALModule):
+    """ A simple module able to react to sensor events.
+    Leave this doc string else this module will not be bound!
+
+    """
+    def __init__(self, name):
+        ALModule.__init__(self, name)
+        # No need for IP and port here because
+        # we have our Python broker connected to NAOqi broker
+        self.memory = ALProxy("ALMemory")
+        # Unsubscribe to event if still subscribed.
+        try:
+            self.memory.unsubscribeToEvent("TouchChanged", "ReactToTouch")
+        except Exception, e:
+            pass
+        self.memory.subscribeToEvent("TouchChanged", "ReactToTouch", "onTouched")
+        self.touched = ""
+    
+    def onTouched(self, strVarName, value):
+        self.touched = value[0][0]
+
+    def getTouch(self):
+        if self.touched != "":
+            tmpTouched = self.touched
+            self.touched = ""
+            return tmpTouched
+        else:
+            return ""
+
+
+class speechRecognitionModule(ALModule):
     """ A simple module able to react to speech events.
     Leave this doc string else this module will not be bound!
 
     """
     def __init__(self, name):
-        global memorySpeech
         ALModule.__init__(self, name)
         # No need for IP and port here because
         # we have our Python broker connected to NAOqi broker
-        memorySpeech = ALProxy("ALMemory")
+        self.memory = ALProxy("ALMemory")
         # Unsubscribe to event if still subscribed to stop ASR engine.
         try:
-            memorySpeech.unsubscribeToEvent("WordRecognized", "SpeechRecognizer")
+            self.memory.unsubscribeToEvent("WordRecognized", "SpeechRecognizer")
         except Exception, e:
             pass
         self.asr = ALProxy("ALSpeechRecognition")
@@ -40,18 +70,16 @@ class speechRecognizerModule(ALModule):
         self.word = ""
     
     def startListening(self):
-        global memorySpeech
         # Subscribe to the WordRecognized event:
         try:
-            memorySpeech.subscribeToEvent("WordRecognized", "SpeechRecognizer", "onWordRecognized")
+            self.memory.subscribeToEvent("WordRecognized", "SpeechRecognizer", "onWordRecognized")
         except Exception, e:
             pass
 
     def stopListening(self):
-        global memorySpeech
-        # Subscribe to the WordRecognized event:
+        # Unsubscribe to the WordRecognized event:
         try:
-            memorySpeech.unsubscribeToEvent("WordRecognized", "SpeechRecognizer")
+            self.memory.unsubscribeToEvent("WordRecognized", "SpeechRecognizer")
         except Exception, e:
             pass
 
@@ -64,55 +92,94 @@ class speechRecognizerModule(ALModule):
             return ""
 
     def onWordRecognized(self, key, value, message):
-        global memorySpeech
         if(len(value) > 1 and value[1] >= 0.3):
             # Stop speech recognition.
             self.stopListening()
             self.word = value[0]
 
-class HumanGreeterModule(ALModule):
+class FaceRecognitionModule(ALModule):
     """ A simple module able to react to facedetection events.
     Leave this doc string else this module will not be bound!
 
     """
     def __init__(self, name):
-        global memoryFace
         ALModule.__init__(self, name)
         # No need for IP and port here because
         # we have our Python broker connected to NAOqi broker
-        memoryFace = ALProxy("ALMemory")
+        self.memory = ALProxy("ALMemory")
         # Unsubscribe to event if still subscribed.
         try:
-            memoryFace.unsubscribeToEvent("FaceDetected", "HumanGreeter")
+            self.memory.unsubscribeToEvent("FaceDetected", "FaceRecognizer")
         except Exception, e:
             pass
+        self.tracking = ALProxy("ALTracker")
         self.timeFaceDetectionStartedInSeconds = 0
         self.timeFirstRecognitionInSeconds = 0
         self.previousTimeInSeconds = 0
+        self.timeInSeconds = 0
         self.face = ""
+        self.tracker = ALProxy("ALTracker")
+        self.motion = ALProxy("ALMotion")
         self.leds = ALProxy("ALLeds")
+        
+    def learnFace(self, face):
+        # Create a proxy to ALFaceDetection
+        try:
+            faceProxy = ALProxy("ALFaceDetection")
+            # First forget face to be enable learning it again.
+            faceProxy.forgetPerson(face)
+            faceProxy.learnFace(face)
+        except Exception, e:
+            print str(e)
+
+
+    def unlearnAllFaces(self):
+        # Create a proxy to ALFaceDetection
+        try:
+            faceProxy = ALProxy("ALFaceDetection")
+            faceProxy.clearDatabase()
+        except Exception, e:
+            print str(e)
+
     
     def startFaceDetection(self):
-        global memorySpeech
-        # Subscribe to the WordRecognized event:
+        # Subscribe to the FaceDetected event:
         try:
             self.timeFaceDetectionStartedInSeconds = int(time.time())
-            memoryFace.subscribeToEvent("FaceDetected", "HumanGreeter", "onFaceDetected")
+            self.memory.subscribeToEvent("FaceDetected", "FaceRecognizer", "onFaceDetected")
+            # Set eye leds.
             self.leds.fadeRGB("FaceLeds", 255*256*0 + 256*0 + 255, 0.2)
         except Exception, e:
-            pass
+            print str(e)
 
     def stopFaceDetection(self):
-        global memorySpeech
-        # Subscribe to the WordRecognized event:
+        # Unsubscribe to the FaceDetected event:
         try:
-            memoryFace.unsubscribeToEvent("FaceDetected", "HumanGreeter")
+            self.memory.unsubscribeToEvent("FaceDetected", "FaceRecognizer")
             self.leds.fadeRGB("FaceLeds", 255*256*100 + 256*100 + 100, 0.2)
         except Exception, e:
-            pass
+            print str(e)
+
+    def startFaceTracking(self):
+        try:
+            # Start face tracking.
+            self.tracker.registerTarget("Face", 0.2)
+            self.tracker.setMode("Head")  # Only move head.
+            self.tracker.track("Face")
+            # Set stiffness of head to 1.0 as this is required for face tracking.
+            self.motion.setStiffnesses("Head", 1.0)
+        except Exception, e:
+            print str(e)
+
+    def stopFaceTracking(self):
+        try:
+            # Stop face tracking.
+            self.tracker.stopTracker()
+            self.tracker.unregisterAllTargets()
+        except Exception, e:
+            print str(e)
 
     def getFace(self):
-        global memoryFace
         if self.face != "":
             tmpFace = self.face
             self.face = ""
@@ -125,14 +192,13 @@ class HumanGreeterModule(ALModule):
         detected.
         Leave this doc string else this method will not be bound!
         """
-        global memoryFace, timeInSeconds
-        val = memoryFace.getData("FaceDetected")
+        val = self.memory.getData("FaceDetected")
         if(val and isinstance(val, list) and len(val) >= 2):
             # We detected faces !
             # For each face, we can read its shape info and ID.
             # First Field = TimeStamp.
             # Second Field = array of face_Info's.
-            timeInSeconds = val[0][0]
+            self.timeInSeconds = val[0][0]
             # After calling subscribeToEvent() the face detection module will generate some events.
             # The reason is unknown, they might be older events from just before the call to unsubscribeToEvent(), although the timestamp
             # is new.
@@ -141,8 +207,7 @@ class HumanGreeterModule(ALModule):
                 return
             # Set timeFirstRecognitionInSeconds if this is the start of a new recognition.
             if self.timeFirstRecognitionInSeconds == 0:
-                print "**************** started"
-                self.timeFirstRecognitionInSeconds = timeInSeconds
+                self.timeFirstRecognitionInSeconds = self.timeInSeconds
             faceInfoArray = val[1]
             try:
                 faceInfo = faceInfoArray[0]
@@ -151,7 +216,7 @@ class HumanGreeterModule(ALModule):
                 faceShapeInfo = faceInfo[0]
                 # Second Field = Extra info (empty for now).
                 faceExtraInfo = faceInfo[1]
-                print "face detected!, confidence = " + str(faceExtraInfo[1]) + ", length = " + str(len(Time_Filtered_Reco_Info))
+                #print "face detected!, confidence = " + str(faceExtraInfo[1]) + ", length = " + str(len(Time_Filtered_Reco_Info))
                 if faceExtraInfo[1] > 0.5:
                     # Stop face detection.
                     self.stopFaceDetection()
@@ -159,89 +224,78 @@ class HumanGreeterModule(ALModule):
                     # Indicate the end of this recognition.
                     self.timeFirstRecognitionInSeconds = 0
                 else:
-                    if self.timeFirstRecognitionInSeconds != 0 and timeInSeconds - self.timeFirstRecognitionInSeconds > 3:
+                    if self.timeFirstRecognitionInSeconds != 0 and self.timeInSeconds - self.timeFirstRecognitionInSeconds > 3:
                         # Face is not recognized for more then three seconds, so we consider it to be unknown.
                         # Stop face detection.
                         self.stopFaceDetection()
                         self.face = "unknown"
                         # Indicate the end of this recognition.
                         self.timeFirstRecognitionInSeconds = 0
-                self.previousTimeInSeconds = timeInSeconds
+                self.previousTimeInSeconds = self.timeInSeconds
         
             except Exception, e:
                 print str(e)
 
 
-def learnFace(face):
-    # Create a proxy to ALFaceDetection
-    try:
-      faceProxy = ALProxy("ALFaceDetection")
-      # First forget face to be enable learning it again.
-      faceProxy.forgetPerson(face)
-      faceProxy.learnFace(face)
-    except Exception, e:
-      print str(e)
-
-
-def unlearnAllFaces():
-    # Create a proxy to ALFaceDetection
-    try:
-      faceProxy = ALProxy("ALFaceDetection")
-      faceProxy.clearDatabase()
-    except Exception, e:
-      print str(e)
-
-
-def main():
-    """ Main entry point
-
-    """
-    global SpeechRecognizer, HumanGreeter
-
-    # We need this broker to be able to construct
-    # NAOqi modules and subscribe to other modules
-    # The broker must stay alive until the program exists
-    myBroker = ALBroker("myBroker",
-       "0.0.0.0",   # listen to anyone
-       0,           # find a free port and use it
-       "nao.local",         # parent broker IP
-       9559)       # parent broker port
-
-
-    # Warning: HumanGreeter must be a global variable
-    # The name given to the constructor must be the name of the
-    # variable
-    # First kill previous instance of HumanGreeter if needed.
-    p = None
-    try:
-        p = ALProxy("HumanGreeter")
-    except Exception, e:
+# Remove this dummy GeneratedClass() when running in Choregraphe.
+class GeneratedClass():
+    def __init__(self):
         pass
-    if p:
-        p.exit() # kill previous instance
 
-    SpeechRecognizer = speechRecognizerModule("SpeechRecognizer")
-    HumanGreeter = HumanGreeterModule("HumanGreeter")
-    tts = ALProxy("ALTextToSpeech")
 
-    try:
-        unlearnAllFaces()
-        while True:
-            tts.say("let's greet some people")
-            HumanGreeter.startFaceDetection()
+class MyClass(GeneratedClass):
+    def __init__(self):
+        global SpeechRecognizer, FaceRecognizer, ReactToTouch
+        GeneratedClass.__init__(self)
+        # We need this broker to be able to construct
+        # NAOqi modules and subscribe to other modules
+        # The broker must stay alive until the program exists
+
+
+        # Warning: module names must be global variables.
+        # The name given to the constructor must be the name of the variable.
+        SpeechRecognizer = speechRecognitionModule("SpeechRecognizer")
+        FaceRecognizer = FaceRecognitionModule("FaceRecognizer")
+        ReactToTouch = ReactToTouchModule("ReactToTouch")
+        self.tts = ALProxy("ALTextToSpeech")
+
+    def onLoad(self):
+        #put initialization code here
+        pass
+        
+    def onUnload(self):
+        # put clean-up code here
+        pass
+    
+    def onInput_onStart(self):
+        global SpeechRecognizer, FaceRecognizer, ReactToTouch
+        FaceRecognizer.unlearnAllFaces()
+        doContinue = True
+        self.tts.say("let's greet some people")
+        FaceRecognizer.startFaceTracking()
+        while doContinue:
+            FaceRecognizer.startFaceDetection()
             face = ""
             while True:
-                face = HumanGreeter.getFace();
+                # Possibility to interrupt by touch.
+                if re.search('.*bumper.*', ReactToTouch.getTouch(), re.IGNORECASE):
+                    doContinue = False
+                    break
+                face = FaceRecognizer.getFace();
                 if face != "":
                     break
                 time.sleep(0.5)
                 
-            if face == "unknown":
-                tts.say("what is your name?")
+            if doContinue and face == "unknown":
+                self.tts.say("what is your name?")
                 SpeechRecognizer.startListening()
                 startTime = int(time.time())
                 name = ""
                 while True:
+                    # Possibility to interrupt by touch.
+                    if re.search('.*bumper.*', ReactToTouch.getTouch(), re.IGNORECASE):
+                        doContinue = False
+                        break
                     if int(time.time()) - startTime > 5:
                         # Timeout.
                         SpeechRecognizer.stopListening()
@@ -250,20 +304,47 @@ def main():
                     if name != "":
                         break
                     time.sleep(0.5)
+            
+                if doContinue and name != "":
+                    self.tts.say("nice to meet you, " + name + ",please keep still for a moment")
+                    FaceRecognizer.learnFace(name)
+                    self.tts.say("thank you, I will remember you!")
+            elif doContinue:
+                self.tts.say("hi again," + face)
 
-                if name != "":
-                    tts.say("nice to meet you, " + name + ",please keep still for a moment")
-                    print "going to learn face"
-                    learnFace(name)
-                    print "ready to learn face"
-                    tts.say("thank you, I will remember you!")
-            else:
-                tts.say("hi again," + face)
+        # Important to call exit on the modules created, otherwise the next time ALModule.__init__(self, name)
+        # will fail because the modules are already registered!
+        FaceRecognizer.stopFaceDetection()
+        FaceRecognizer.stopFaceTracking()
+        SpeechRecognizer.stopListening()
+        SpeechRecognizer.exit()
+        FaceRecognizer.exit()
+        ReactToTouch.exit()
+        # Uncomment the below line in Choregraphe.
+        #self.onStopped() #activate the output of the box
 
-    except Exception, e:
-        print str(e)
-        myBroker.shutdown()
-        sys.exit(0)
+    def onInput_onStop(self):
+        self.onUnload() #it is recommended to reuse the clean-up as the box is stopped
+        self.onStopped() #activate the output of the box
+
+
+# Remove main() when running in Choregraphe.
+def main():
+    """ Main entry point
+
+    """
+    # We need this broker to be able to construct
+    # NAOqi modules and subscribe to other modules
+    # The broker must stay alive until the program exists
+    myBroker = ALBroker("myBroker",
+        "0.0.0.0",   # listen to anyone
+        0,           # find a free port and use it
+        "nao.local",         # parent broker IP
+        9559)       # parent broker port
+
+    myClass = MyClass()
+    myClass.onInput_onStart()
+    myClass.onUnload()
 
 
 if __name__ == "__main__":
